f0540f2943b43a90f07daf9d2d51f8e0c60f9d8f
Greg.Macdonald <greg.macdonald@digital.hmrc.gov.uk>
2024-05-15 14:51:14 +0100

    Added Extent reporting

--- OrangeHRMDotNet6TestProject/Steps/Shared/Hooks.cs
+++ OrangeHRMDotNet6TestProject/Steps/Shared/Hooks.cs
@@ -6,6 +6,10 @@ using System.Threading.Tasks;
 
 using OrangeHRMDotNet6TestProject.Utilities; //Location of Driver class
 using TechTalk.SpecFlow;
+using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+using AventStack.ExtentReports.Model;
+using Gherkin.CucumberMessages.Types;
 
 
 namespace OrangeHRMDotNet6TestProject.Steps.Shared
@@ -13,16 +17,49 @@ namespace OrangeHRMDotNet6TestProject.Steps.Shared
     [Binding]
     public class Hooks
     {
+        private static ExtentReports _extent;
+        private static ExtentTest _test;
+
+        [BeforeTestRun]
+        private static void InitialiseReport()
+        {
+            ExtentSparkReporter spark = new ExtentSparkReporter(@"..\..\Reports\testReport.html");
+
+            _extent = new ExtentReports();
+            _extent.AttachReporter(spark);
+
+        }
+        [AfterTestRun]
+        public static void AfterTestRun()
+        {
+            //Instructs ExtentReports write the test information to a destination
+            //You will not get any report without this line!
+            _extent.Flush();
+        }
+        
         [BeforeScenario()]
         public static void StartBrowser()
         {
             Driver.RootURL = Driver.GetBaseURL();
             Driver.OpenBrowser(Driver.GetBrowser());
+
+            _test = _extent.CreateTest(ScenarioContext.Current.ScenarioInfo.Title);
         }
 
         [AfterScenario()]
         public static void ShutDown()
         {
+
+            // Logic to capture test result and add it to the report
+            if (ScenarioContext.Current.TestError != null)
+            {
+                _test.Fail(ScenarioContext.Current.TestError.Message);
+            }
+            else
+            {
+                _test.Pass("Test passed!");
+             }
+
             Driver.ShutDown();
         }
     }
